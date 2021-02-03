84bcb3f7b92729ea99c87b43ea46fc1fc23d0efe
Raul <64620968+RaulMunozSaucedoJr@users.noreply.github.com>
2021-02-03 13:25:47 -0600

    first commit

---
+++ Scripts.js
@@ -0,0 +1,16 @@
+! function () {
+    "use strict";
+    document.addEventListener("DOMContentLoaded", function () {
+        $(document).ready(function () {
+            $(".fixed-top").click(function () {
+                $(".menu").slideToggle(400)
+            })
+        }), window.addEventListener("scroll", () => {
+            document.querySelector("#navbar").classList[window.scrollY > 50 ? "add" : "remove"]("hide")
+        });
+        const e = document.querySelector(".to-top");
+        window.addEventListener("scroll", () => {
+            window.pageYOffset > 300 ? e.classList.add("active") : e.classList.remove("active")
+        });
+    });
+}();
