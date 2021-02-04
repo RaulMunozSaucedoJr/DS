714ceb0c34eac7428a7314d5d68ed441438ab822
Raul <64620968+RaulMunozSaucedoJr@users.noreply.github.com>
2021-02-03 23:28:11 -0600

    VJS

--- Validacion.js
+++ Validacion.js
@@ -9,36 +9,11 @@ function Validacion(event) {
     const numero = document.getElementById("numeroCliente");
     const mensaje = document.getElementById("mensajeCliente");
 
-    expresionCorreo = /^[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])$/;
-    expresionTelefono = /^\d([0-9]{1,3})+([0-9]{1,4})+([0-9]{1,4})+$/;
-
     if (nombre.length === "" || direccion.length === "" || correo.length === "" || numero.length === "" || mensaje.length === "") {
         swal({
             title: "¡ATENCIÓN!",
             text: "¡Ningún campo debe de permanecer vacio!.",
             icon: "warning",
-            timer: 4000,
-            closeOnClickOutside: false,
-            button: false,
-            closeOnEsc: true,
-        });
-        return false;
-    } else if (!expresionCorreo.test(correo)) {
-        swal({
-            title: "¡ATENCIÓN!",
-            text: "Ingrese un correo electrónico valido.",
-            icon: "info",
-            timer: 4000,
-            closeOnClickOutside: false,
-            button: false,
-            closeOnEsc: true,
-        });
-        return false;
-    } else if (!expresionTelefono.test(numero)) {
-        swal({
-            title: "¡ATENCIÓN!",
-            text: "Ingrese un numero telefónico valido.",
-            icon: "info",
             timer: 4000,
             closeOnClickOutside: false,
             button: false,
